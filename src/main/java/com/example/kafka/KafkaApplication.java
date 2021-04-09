169eee52bae0dc55eb7306f571f2dddf550ec7fc
Demo-Sonarqube <sambhav.184Ca068@nitk.edu.in>
2021-04-09 13:19:25 +0530

    Update KafkaApplication.java

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -3,6 +3,7 @@ import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Vector;
+import java.util.Map;
 @SpringBootApplication
 public class  KafkaApplication {
 
