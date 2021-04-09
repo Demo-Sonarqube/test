891a927466547e3cc621e4d1b0b76972a65ed2cb
Demo-Sonarqube <sambhav.184Ca068@nitk.edu.in>
2021-04-09 13:36:04 +0530

    Update KafkaApplication.java

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -6,6 +6,7 @@ import java.util.Vector;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class  KafkaApplication {
